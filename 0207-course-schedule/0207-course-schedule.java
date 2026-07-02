23564311268fa5845d22e6c70c4ae376e42125d6
anushkaptl07 <anushkapatil1910@gmail.com>
2026-07-02 20:59:36 +0530

    Time: 7 ms (57.73%), Space: 47.4 MB (13.6%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean canFinish(int V, int[][] course) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0;i<V;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0;i<course.length;i++)
+        {
+            adj.get(course[i][1]).add(course[i][0]);
+        }
+        int[] indegree = new int[V];
+        for(int i=0;i<V;i++)
+        {
+            for(int it:adj.get(i))
+            {
+                indegree[it]++;
+            }
+        }
+        Queue<Integer> q=new LinkedList<>();
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.offer(i);
+            }
+        }
+        List<Integer> topo = new ArrayList<>();
+        while(!q.isEmpty())
+        {
+          int curr = q.poll();
+          topo.add(curr);
+          for(int it:adj.get(curr))
+          {
+            indegree[it]--;
+            if(indegree[it]==0)
+            {
+                q.offer(it);
+            }
+          }
+        }
+        if(topo.size()==V) return true;
+        return false;
+    }
+}
